5eee99b6e093e6f6d9a3bf0c54925bd0642bca8c
Daniel Santos <dsantosp12@gmail.com>
2017-10-19 01:00:27 -0400

    removed view capabilities

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,7 @@
 import App
-import LeafProvider
 
 let config = try Config()
 try config.setup()
-try config.addProvider(LeafProvider.Provider.self)
 
 let drop = try Droplet(config)
 try drop.setup()
